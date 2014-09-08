56001e777ee5b9d79526123b91f0e776309e8e11
RouxRC <b.ooghe@gmail.com>
2014-09-08 06:12:36 +0200

    better format for umap

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 ./list-agences.py > agences.csv
 
-grep -P "\t(Code Postal|75\d\d\d)\t" agences.tsv |
-  awk -F "\t" '{print "\""$1"\",\""$2"\",\""$3"\","$4","$5","$6}' > agences-paris.csv
+echo '"nom","description",lat,lon' > agences-paris.csv
+grep -P "\t(75\d\d\d)\t" agences.tsv |
+  awk -F "\t" '{print "\""$1"\",\""$2" "$4"\n"$3"\","$5","$6}' >> agences-paris.csv
 
